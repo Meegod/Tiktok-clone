26f77020d53993dc819085c345ca043afb9ab235
Godfrey <[godfreyfreezey@gmail.com]>
2021-03-18 11:27:25 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Videocard from './Videocard';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className="app__top">
+   {/*image at the top*/}
+   <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ffr.followersnet.com%2Fwp-content%2Fuploads%2F2019%2F01%2FTikTok.png&f=1&nofb=1"
+   className="app__logo"/>
+   
+   <h1 className="app__text">Reels</h1>
+   </div>
+   <div className="app__videos">
+   {/*Container of app__videos()scrollable container*/}
+    
+<Videocard />
+
+
+
+   </div>
+
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Videocard.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Videocard.css';
+
+
+
+function Videocard(){
+
+  return(
+     <div className="videoCard">
+     <video 
+     className="videoCard__player"
+     src="https://www.instagram.com/p/CMW9mNGjiHi/?utm_source=ig_web_copy_link"
+     alt="Tik tok"
+     loop/>
+
+
+
+
+     </div>
+
+
+
+
+
+  	)
+
+
+
+}
+export default Videocard
